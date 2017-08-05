c8a888eb6bddc70214664191d5740c82e89b871f
Will保哥 <doggy.huang@gmail.com>
2017-08-05 15:14:22 +0800

    10 練習不同的傳參數方法 ( Optional Parameters ) - Matrix URI v.s. Query String

--- src/app/cards/cards.component.ts
+++ src/app/cards/cards.component.ts
@@ -9,12 +9,20 @@ import { Component, OnInit } from '@angular/core';
 export class CardsComponent implements OnInit {
 
   type: number = 0;
+  action: string = '';
+  action1: string = '';
+
   constructor(private router: Router, private route: ActivatedRoute) {}
 
   ngOnInit() {
     // this.type = this.route.snapshot.params['type'];
     this.route.params.subscribe(params => {
       this.type = params['type'];
+      this.action = params['action'];
+    });
+
+    this.route.queryParams.subscribe(params => {
+      this.action1 = params['action1'];
     });
   }
 
