ef71190c8bae3252567421247fca6434167e133b
Will保哥 <doggy.huang@gmail.com>
2017-08-05 16:03:14 +0800

    11 建立 ChartsModule 並與 AppModule 分離乾淨並實現子模組延遲載入機制

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,7 +5,6 @@ import { Page2Component } from './page2/page2.component';
 import { fallbackRoute } from './shared/fallback-route';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { CardsComponent } from './cards/cards.component';
-import { FlotComponent } from './charts/flot/flot.component';
 
 const routes: Routes = [
   { path: '', redirectTo: 'dashboard', pathMatch: 'full' },
