e8b34edf379846a0df4383141fc908744262f920
Will保哥 <doggy.huang@gmail.com>
2017-08-05 10:41:09 +0800

    示範轉向路由與萬用路由

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,9 +4,10 @@ import { Page1Component } from './page1/page1.component';
 import { Page2Component } from './page2/page2.component';
 
 const routes: Routes = [
-  { path: '', component: Page1Component },
+  { path: '', redirectTo: 'page1', pathMatch: 'full' },
   { path: 'page1', component: Page1Component },
-  { path: 'page2', component: Page2Component }
+  { path: 'page2', component: Page2Component },
+  { path: '**', redirectTo: '/', pathMatch: 'full' }
 ];
 
 @NgModule({
