633c15ea2058851af1a6eb0cdf0ea7df2f6cd5ed
Will保哥 <doggy.huang@gmail.com>
2017-08-05 10:46:20 +0800

    04 練習預設路由、轉向路由、萬用路由與對程式碼進行整理

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,12 +2,7 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { Page1Component } from './page1/page1.component';
 import { Page2Component } from './page2/page2.component';
-
-const fallbackRoute = {
-  path: '**',
-  redirectTo: '/',
-  pathMatch: 'full'
-};
+import { fallbackRoute } from './shared/fallback-route';
 
 const routes: Routes = [
   { path: '', redirectTo: 'page1', pathMatch: 'full' },
