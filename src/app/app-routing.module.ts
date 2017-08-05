88139207a5becabb751702cb38837243059c53e3
Will保哥 <doggy.huang@gmail.com>
2017-08-05 11:07:54 +0800

    示範注入 Router 與 Route 的基本用法

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,7 +6,7 @@ import { fallbackRoute } from './shared/fallback-route';
 
 const routes: Routes = [
   { path: '', redirectTo: 'page1', pathMatch: 'full' },
-  { path: 'page1', component: Page1Component },
+  { path: 'page1', component: Page1Component, data: { title: 'My Page1'} },
   { path: 'page2', component: Page2Component },
   fallbackRoute
 ];
--- src/app/page1/page1.component.ts
+++ src/app/page1/page1.component.ts
@@ -1,3 +1,4 @@
+import { Router, ActivatedRoute } from '@angular/router';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -7,9 +8,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class Page1Component implements OnInit {
 
-  constructor() { }
+  title: string;
+
+  constructor(private router: Router, private route: ActivatedRoute) { }
 
   ngOnInit() {
+    this.title = this.route.snapshot.data['title'];
   }
 
 }
