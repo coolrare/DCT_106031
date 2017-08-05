a83d7813353659fd1b3d7c732326b71e18d40b7f
Will保哥 <doggy.huang@gmail.com>
2017-08-05 17:03:10 +0800

    12 建立 Login 元件並調整路由結構讓 Login 可以完整顯示在頁面上

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,14 +5,20 @@ import { Page2Component } from './page2/page2.component';
 import { fallbackRoute } from './shared/fallback-route';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { CardsComponent } from './cards/cards.component';
+import { LoginComponent } from './login/login.component';
+import { LayoutComponent } from './layout/layout.component';
 
 const routes: Routes = [
-  { path: '', redirectTo: 'dashboard', pathMatch: 'full' },
-  { path: 'dashboard', component: DashboardComponent },
-  { path: 'cards/:type', component: CardsComponent },
-  { path: 'charts',
-    loadChildren: './charts/charts.module#ChartsModule'
-  },
+  { path: 'login', component: LoginComponent },
+  { path: '', component: LayoutComponent,
+    children: [
+      { path: '', redirectTo: 'dashboard', pathMatch: 'full' },
+      { path: 'dashboard', component: DashboardComponent },
+      { path: 'cards/:type', component: CardsComponent },
+      { path: 'charts',
+        loadChildren: './charts/charts.module#ChartsModule'
+      }
+  ]},
   fallbackRoute
 ];
 
